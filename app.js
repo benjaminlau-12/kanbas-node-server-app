b3f82562919abe54a45189854d4a20ce442151ac
benjaminlau-12 <lau.be@northeastern.edu>
2023-11-21 00:19:56 -0500

    All should be good

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import Lab5 from './Lab5.js';
 import CourseRoutes from './courses/routes.js';
 import ModuleRoutes from './modules/routes.js';
 import cors from "cors";
+import "dotenv/config";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
