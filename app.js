3920a6b5beada6b7a85786b901ddd2b787cd9311
benjaminlau-12 <lau.be@northeastern.edu>
2023-12-04 22:48:21 -0500

    Finished until section 5

--- app.js
+++ app.js
@@ -5,10 +5,42 @@ import CourseRoutes from './courses/routes.js';
 import ModuleRoutes from './modules/routes.js';
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+import session from 'express-session';
+import "dotenv/config";
 
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB at localhost');
+});
 const app = express();
-app.use(cors());
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+    })
+);
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(session(sessionOptions));
+
+
+
 app.use(express.json());
+UserRoutes(app);
 Hello(app);
 ModuleRoutes(app);
 CourseRoutes(app);
